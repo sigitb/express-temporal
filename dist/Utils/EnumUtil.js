b9e6b319d2caca1de7fde167fd06afafbb14d49d
sigit <sigitbudianto423@gmail.com>
2024-05-13 11:11:38 +0700

    delete build

--- dist/Utils/EnumUtil.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SyncType = void 0;
-var SyncType;
-(function (SyncType) {
-    SyncType["PRODUCT"] = "PRODUCT";
-    SyncType["SUPPLIER"] = "SUPPLIER";
-})(SyncType || (exports.SyncType = SyncType = {}));
